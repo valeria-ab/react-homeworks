bc20b9898babdd8c22f779664a5050b8c6624af8
valeria-ab <lera.magnum@gmail.com>
2021-11-04 16:39:14 +0300

    dz-9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,31 +1,31 @@
-import React, {useState} from "react";
-import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+import React, { useState } from 'react'
+import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
-    const [timerId, setTimerId] = useState<number>(0);
-    const [date, setDate] = useState<Date>();
-    const [show, setShow] = useState<boolean>(false);
+    const [timerId, setTimerId] = useState<number>(0)
+    const [date, setDate] = useState<Date>()
+    const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
-        stop();
-        const id: number = window.setInterval(() => {
-            // setDate
-        }, 1000);
-        setTimerId(id);
+        stop()
+        const id: number = +setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+        setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
-    };
+        setShow(true)
+    }
     const onMouseLeave = () => {
-        // close
-    };
+        setShow(false)
+    }
 
-    const stringTime = "Time"; // fix with date
-    const stringDate = "Date"; // fix with date
+    const stringTime = date?.toLocaleTimeString() || <br />
+    const stringDate = date?.toLocaleDateString() || <br />
 
     return (
         <div>
@@ -36,17 +36,22 @@ function Clock() {
                 {stringTime}
             </div>
 
-            {show && (
-                <div>
-                    {stringDate}
-                </div>
-            )}
+            {show
+                ? (
+                    <div>
+                        {stringDate}
+                    </div>
+                )
+                : (
+                    //br это заглушка если нет дивки, у него такая же высота как у div (при стандартном шрифте)
+                    <br />
+                )}
 
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
 
         </div>
-    );
+    )
 }
 
-export default Clock;
+export default Clock
--- src/p2-homeworks/h9/HW9.tsx
+++ src/p2-homeworks/h9/HW9.tsx
@@ -1,21 +1,17 @@
-import React from "react";
-import Clock from "./Clock";
+import React from 'react'
+import Clock from './Clock'
 
 function HW9() {
     return (
         <div>
             <hr/>
-            homeworks 9
+            <h4>homeworks 9</h4>
 
-            {/*should work (должно работать)*/}
             <Clock/>
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeClock/>*/}
-            <hr/>
+        
         </div>
-    );
+    )
 }
 
-export default HW9;
+export default HW9
